c0f03e8dbaee2b73c0c73e1a9caff29b9965d010
Dodai Ukoko <ukokododai69@gmail.com>
2026-05-18 12:19:13 +0100

    Added test class

---
+++ src/test/java/com/example/relationship/service/WalletServiceTest.java
@@ -0,0 +1,40 @@
+package com.example.relationship.service;
+
+import com.example.relationship.exception.EntityNotFoundException;
+import com.example.relationship.repository.WalletRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class WalletServiceTest {
+
+    @Mock
+    WalletRepository walletRepository;
+
+    @InjectMocks
+    WalletService walletService;
+
+    @Test
+    void walletIdShouldBePositive() {
+        Long invalidId = -5L;
+        when(walletRepository.findById(invalidId)).thenReturn(Optional.empty());
+
+        assertThrows(EntityNotFoundException.class, () -> walletService.findById(invalidId));
+    }
+
+    @Test
+    void walletIdShouldNotBeNull(){
+        when(walletRepository.findById(null)).thenReturn(Optional.empty());
+
+        assertThrows(EntityNotFoundException.class, () -> walletService.findById(null));
+    }
+
+}
